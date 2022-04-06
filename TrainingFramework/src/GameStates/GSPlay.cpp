a09d27c66405dc144ed9535b7f8faa67b4d0af57
namdc2k <namdc4920@gmail.com>
2022-04-06 23:00:55 +0700

    check gameplay

--- TrainingFramework/src/GameStates/GSPlay.cpp
+++ TrainingFramework/src/GameStates/GSPlay.cpp
@@ -46,6 +46,12 @@ void GSPlay::Init()
 	m_base2->Set2DPosition(Vector2((float)Globals::screenWidth / 2, 770));
 	m_base2->SetSize(480, 238);
 
+	// pause background
+	texture = ResourceManagers::GetInstance()->GetTexture("GSPlay_bg.tga");
+	m_pause_bg = std::make_shared<Sprite2D>(model, shader, texture);
+	m_pause_bg->Set2DPosition(Globals::screenWidth / 2, Globals::screenHeight / 2);
+	m_pause_bg->SetSize(300, 300);
+
 	// tree
 	texture = ResourceManagers::GetInstance()->GetTexture("tree_up.tga");
 	treeUp = std::make_shared<Sprite2D>(model, shader, texture);
@@ -53,24 +59,56 @@ void GSPlay::Init()
 	treeUp->SetSize(89, 401);
 	texture = ResourceManagers::GetInstance()->GetTexture("tree_down.tga");
 	treeDown = std::make_shared<Sprite2D>(model, shader, texture);
-	treeDown->Set2DPosition(Vector2(800, 600));
+	treeDown->Set2DPosition(Vector2(800, 630));
 	treeDown->SetSize(89, 401);
 
-	// button close
-	texture = ResourceManagers::GetInstance()->GetTexture("btn_close.tga");
+	// button pause
+	texture = ResourceManagers::GetInstance()->GetTexture("btn_pause.tga");
 	std::shared_ptr<GameButton>  button = std::make_shared<GameButton>(model, shader, texture);
 	button->Set2DPosition(Globals::screenWidth - 50, 50);
 	button->SetSize(50, 50);
 	button->SetOnClick([this]() {
-		GameStateMachine::GetInstance()->PopState();
+		status_pause = !status_pause;
 		});
 	m_listButton.push_back(button);
+
+	// close
+	texture = ResourceManagers::GetInstance()->GetTexture("btn_close.tga");
+	m_close = std::make_shared<GameButton>(model, shader, texture);
+	m_close->Set2DPosition(Globals::screenWidth / 2 - 50, Globals::screenHeight / 2);
+	m_close->SetSize(50, 50);
+	m_close->SetOnClick([this]() {
+		GameStateMachine::GetInstance()->PopState();
+		});
+	// restart
+
+	texture = ResourceManagers::GetInstance()->GetTexture("btn_restart.tga");
+	m_restart = std::make_shared<GameButton>(model, shader, texture);
+	m_restart->Set2DPosition(Globals::screenWidth / 2 + 50, Globals::screenHeight / 2);
+	m_restart->SetSize(50, 50);
+	m_restart->SetOnClick([this]() {
+		GameStateMachine::GetInstance()->PopState();
+		GameStateMachine::GetInstance()->ChangeState(StateType::STATE_PLAY);
+		});
+
 
 	// score
 	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
 	std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("AngryBirds.ttf");
 	m_score = std::make_shared< Text>(shader, font, "score: 0", TextColor::RED, 1.5f);
 	m_score->Set2DPosition(Vector2(5, 64));
+
+	// text pause
+	std::shared_ptr<Text> text_pause = std::make_shared< Text>(shader, font, "restart", TextColor::WHITE, 1.0f);
+	text_pause->Set2DPosition(Globals::screenWidth / 2 + 15, Globals::screenHeight / 2 + 50);
+	m_text_pause.push_back(text_pause);
+
+	text_pause = std::make_shared< Text>(shader, font, "exit", TextColor::WHITE, 1.0f);
+	text_pause->Set2DPosition(Globals::screenWidth / 2 - 75, Globals::screenHeight / 2 + 50);
+	m_text_pause.push_back(text_pause);
+	// gameover
+	m_gameOver = std::make_shared< Text>(shader, font, "Game Over", TextColor::RED, 1.5f);
+	m_gameOver->Set2DPosition(Globals::screenWidth / 2 - 90, Globals::screenHeight / 2 - 70);
 
 	//bird
 	shader = ResourceManagers::GetInstance()->GetShader("Animation");
@@ -107,6 +145,22 @@ void GSPlay::HandleKeyEvents(int key, bool bIsPressed)
 
 void GSPlay::HandleTouchEvents(int x, int y, bool bIsPressed)
 {
+	if (!status_pause) {
+		Vector2 m;
+		for (auto it : m_listAnimation) {
+			if (bIsPressed) {
+				m = it->Get2DPosition();
+				it->Set2DPosition(m.x, m.y - 120);
+				if (Globals::statusSound)
+					ResourceManagers::GetInstance()->PlaySound(soundFLy);
+			}
+		}
+	}
+	else {
+		m_close->HandleTouchEvents(x, y, bIsPressed);
+		m_restart->HandleTouchEvents(x, y, bIsPressed);
+	}
+
 	for (auto button : m_listButton)
 	{
 		if (button->HandleTouchEvents(x, y, bIsPressed))
@@ -114,16 +168,6 @@ void GSPlay::HandleTouchEvents(int x, int y, bool bIsPressed)
 			break;
 		}
 	}
-	Vector2 m;
-	for (auto it : m_listAnimation) {
-		if (bIsPressed) {
-			m = it->Get2DPosition();
-			it->Set2DPosition(m.x, m.y - 120);
-			if (Globals::statusSound)
-				ResourceManagers::GetInstance()->PlaySound(soundFLy);
-		}
-	}
-
 }
 
 void GSPlay::HandleMouseMoveEvents(int x, int y)
@@ -132,41 +176,45 @@ void GSPlay::HandleMouseMoveEvents(int x, int y)
 
 void GSPlay::Update(float deltaTime)
 {
-	Vector3 m1 = m_background->GetPosition();
-	if (m1.x == -240) m1.x = 240 + 480;
-	m_background->Set2DPosition(m1.x - 3, m1.y);
-	m_base->Set2DPosition(m1.x - 3, 770);
-	Vector3 m2 = m_background2->GetPosition();
-	if (m2.x == -240) m2.x = 240 + 480;
-	m_background2->Set2DPosition(m2.x - 3, m2.y);
-	m_base2->Set2DPosition(m2.x - 3, 770);
+	if (!status_pause) {
+		Vector3 m1 = m_background->GetPosition();
+		if (m1.x == -240) m1.x = 240 + 480;
+		m_background->Set2DPosition(m1.x - 3, m1.y);
+		m_base->Set2DPosition(m1.x - 3, 770);
+		Vector3 m2 = m_background2->GetPosition();
+		if (m2.x == -240) m2.x = 240 + 480;
+		m_background2->Set2DPosition(m2.x - 3, m2.y);
+		m_base2->Set2DPosition(m2.x - 3, 770);
 
-	//tree
-	Vector3 m3 = treeUp->GetPosition();
-	if (m3.x <= -45) m3.x = 480 + 45, m3.y = (rand() % 8 - 3) * 30;
-	treeUp->Set2DPosition(m3.x - 5, m3.y);
+		//tree
+		Vector3 m3 = treeUp->GetPosition();
+		if (m3.x <= -45) m3.x = 480 + 45, m3.y = (rand() % 8 - 3) * 30;
+		treeUp->Set2DPosition(m3.x - 5, m3.y);
 
-	Vector3 m4 = treeDown->GetPosition();
-	if (m4.x <= -45) m4.x = 480 + 45, m4.y = m3.y + 600;
-	treeDown->Set2DPosition(m4.x - 5, m4.y);
-	if (m4.x + 45 == (float)Globals::screenWidth / 2) {
-		score++;
-		if (Globals::statusSound)
-			ResourceManagers::GetInstance()->PlaySound("ping.wav");
+		Vector3 m4 = treeDown->GetPosition();
+		if (m4.x <= -45) m4.x = 480 + 45, m4.y = m3.y + 630;
+		treeDown->Set2DPosition(m4.x - 5, m4.y);
+		if (m4.x + 45 == (float)Globals::screenWidth / 2) {
+			score++;
+			if (Globals::statusSound)
+				ResourceManagers::GetInstance()->PlaySound("ping.wav");
+		}
+		Vector2 m;
+		for (auto it : m_listAnimation) {
+			m = it->Get2DPosition();
+			if (m.y > 700)m.y = 700;
+			it->Set2DPosition(m.x, m.y + 300 * deltaTime);
+			it->Update(deltaTime);
+		}
+		if ((m.y <= m3.y + 230 || m.y >= m4.y - 230) && m.x + 97 >= m3.x && m.x + 97 <= m3.x + 194) {
+			status_GO = true;status_pause = true;
+		}
 	}
 	//button
-	for (auto it : m_listButton)
-	{
+	for (auto it : m_listButton) {
 		it->Update(deltaTime);
 	}
-	Vector2 m;
-	for (auto it : m_listAnimation)
-	{
-		m = it->Get2DPosition();
-		if (m.y > 700)m.y = 700;
-		it->Set2DPosition(m.x, m.y + 300 * deltaTime);
-		it->Update(deltaTime);
-	}
+
 	auto toString = [&](GLint score) {
 		std::string ss;
 		while (score > 0) {
@@ -201,5 +249,16 @@ void GSPlay::Draw()
 	for (auto it : m_listAnimation)
 	{
 		it->Draw();
+	}
+	if (status_pause) {
+		m_pause_bg->Draw();
+		m_close->Draw();
+		m_restart->Draw();
+		if (status_GO)
+			m_gameOver->Draw();
+		for (auto it : m_text_pause) {
+			it->Draw();
+		}
+
 	}
 }
--- TrainingFramework/src/GameStates/GSPlay.h
+++ TrainingFramework/src/GameStates/GSPlay.h
@@ -28,14 +28,16 @@ public:
 	int m_Test;
 	
 private:
-	std::shared_ptr<Sprite2D>	m_background, m_background2;
+	std::shared_ptr<Sprite2D>	m_background, m_background2,m_pause_bg;
 	GLint score = 0, tmp = 0;
 	std::string s = "score: 0";
-	std::shared_ptr<Text>		m_score;
+	std::shared_ptr<Text>		m_score, m_gameOver;
+	std::list<std::shared_ptr<Text>>	m_text_pause;
 	std::shared_ptr<Sprite2D>	m_base, m_base2;
 	std::list<std::shared_ptr<GameButton>>	m_listButton;
 	std::shared_ptr<Sprite2D> treeUp, treeDown;
 	std::list<std::shared_ptr<SpriteAnimation>>	m_listAnimation;
-
+	bool status_pause = false, status_GO =false;
+	std::shared_ptr<GameButton> m_close, m_restart;
 };
 
